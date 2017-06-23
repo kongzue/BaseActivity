dc895518e1ac954c0b458b51cd5a41179fb5dea6
ZhangChao <myzcxhh@live.cn>
2017-06-23 16:35:14 +0800

    Create BaseActivityDemo.java

---
+++ BaseActivityDemo.java
@@ -0,0 +1,36 @@
+package com.kongzue.baseframe;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class BaseActivityDemo extends BaseActivity {
+
+    private LinearLayout boxBody;
+    private LinearLayout sysStatusBar;
+    private TextView txtTitle;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState,R.layout.activity_main);
+    }
+
+    @Override
+    public void initViews() {
+        //此处加载组件
+        boxBody = (LinearLayout) findViewById(R.id.box_body);
+        sysStatusBar = (LinearLayout) findViewById(R.id.sys_statusBar);
+        txtTitle = (TextView) findViewById(R.id.txt_title);
+    }
+
+    @Override
+    public void initDatas() {
+        //此处编写初始化代码
+        
+    }
+
+    @Override
+    public void setEvent() {
+        //此处为组件绑定事件
+
+    }
+}
