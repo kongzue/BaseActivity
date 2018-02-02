6df5d1ab6d2a8921f64510eae70c767da7dcbd87
ZhangChao <myzcxhh@live.cn>
2018-02-02 14:47:07 +0800

    Update SystemBarTintManager.java

--- SystemBarTintManager.java
+++ SystemBarTintManager.java
@@ -1,3 +1,25 @@
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout.LayoutParams;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class SystemBarTintManager {
 
     /**
